9c11eb2e381c61a5208c011c93eadf0dfc60ed09
Oleg Kravchenko <okravc@gmail.com>
2023-05-27 22:45:54 +0300

    fix arg defaults

--- gen.sh
+++ gen.sh
@@ -10,8 +10,9 @@ o_step=10
 
 for ((m=m_start; m<=m_end; m+=m_step)); do
   for ((o=o_start; o<=o_end; o+=o_step)); do
-    ./wtcurve -m $m -o $o --wav
+    ./wtcurve -m $m -o $o --wav -B
   done
 done
 
 # for w in *.wav; do ./wttag -s 2048 -w 256 --clm -i $w -o /home/ftp/audio/Wavetables/WT1/My/exponential/$w; done
+# for w in *.wav; do ./wttag -s 2048 -w 256 --clm -i $w -o /home/ftp/audio/Wavetables/WT1/My/bezier/$w; done
