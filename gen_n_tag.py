05250fd8156d4df538e4963cdada9fd5bb09058d
Oleg Kravchenko <okravc@gmail.com>
2023-06-16 17:22:33 +0300

    simplify gen_n_tag

--- gen_n_tag.py
+++ gen_n_tag.py
@@ -36,12 +36,8 @@ def tpath(d):
 
     return types[-1]
 
-
 def mk_wav(d):
-    d['wav'] = True
-    # d.update({'png': True, 'graph': True})
-    # d.update({'gif': True})
-    wtc = wtcurve.WtCurve(d)
+    wtc = wtcurve.WtCurve(d|{'wav': True})
     wtc.generate()
     fn = wtc.fmt_fname('wav')
     dn = os.path.join(wav_path, tpath(d), fn)
@@ -55,9 +51,7 @@ def mk_wav(d):
     tagger.tag()
 
 def mk_wt(d):
-    d['wt'] = True
-    d['bitwidth'] = 16
-    wtc = wtcurve.WtCurve(d)
+    wtc = wtcurve.WtCurve(d|{'wt': True, 'bitwidth': 16})
     wtc.generate()
     fn = wtc.fmt_fname('wt')
     dn = os.path.join(wt_path, tpath(d), fn)
@@ -65,84 +59,70 @@ def mk_wt(d):
 
 
 def mk_h2p(d):
-    d['h2p'] = True
-    wtc = wtcurve.WtCurve(d)
+    wtc = wtcurve.WtCurve(d|{'h2p': True})
     wtc.generate()
     fn = wtc.fmt_fname('h2p')
     dn = os.path.join(h2p_path, tpath(d), fn)
     shutil.move(fn, dn)
 
 def mk_gif(d):
-    d['gif'] = True
-    wtc = wtcurve.WtCurve(d)
+    wtc = wtcurve.WtCurve(d|{'gif': True})
     wtc.generate()
 
 def mk_png(d):
-    d['png'] = True
-    d['graph'] = True
-    wtc = wtcurve.WtCurve(d)
+    wtc = wtcurve.WtCurve(d|{'png': True, 'graph': True})
     wtc.generate()
-
 
 def gen_direct():
     print('variable direct')
-    mid=60
+    mid=90
     ga=35
     for o in range(-25,26,5):
-        #mk_gif({'num_waveforms': 64, 'num_samples': 256, 'mid_yoffset': o,
-        #        'mid_width_pct': mid, 'dline': True})
-        #mk_gif({'num_waveforms': 64, 'num_samples': 256, 'mid_yoffset': o,
-        #        'mid_width_pct': mid, 'dline': True, 'gauss': ga})
-        mk_wav({'num_waveforms': wl, 'num_samples': sa, 'mid_yoffset': o,
-                'mid_width_pct': mid, 'dline': True})
-        mk_wav({'num_waveforms': wl, 'num_samples': sa, 'mid_yoffset': o,
-                'mid_width_pct': mid, 'dline': True, 'gauss': ga})
-        mk_wt({'num_waveforms': wl, 'num_samples': sa_wt, 'mid_yoffset': o,
-               'mid_width_pct': mid, 'dline': True})
-        mk_wt({'num_waveforms': wl, 'num_samples': sa_wt,
-               'mid_width_pct': mid, 'mid_yoffset': o, 'dline': True,
-               'gauss': ga})
-        mk_h2p({'mid_yoffset': o, 'mid_width_pct': mid, 'dline': True})
-        mk_h2p({'mid_yoffset': o, 'mid_width_pct': mid, 'dline': True,
-                'gauss': ga})
+        bdict = {'num_waveforms': wl, 'num_samples': sa, 'dline': True,
+                 'mid_yoffset': o, 'mid_width_pct': mid}
+        # mk_gif(bdict)
+        # mk_gif(bdict|{'gauss': ga})
+        mk_wav(bdict)
+        mk_wav(bdict|{'gauss': ga})
+        mk_wt(bdict|{'num_samples': sa_wt})
+        mk_wt(bdict|{'num_samples': sa_wt, 'gauss': ga})
+        mk_h2p(bdict)
+        mk_h2p(bdict|{'gauss': ga})
 
 def gen_savgol():
-    savgol = (10, 3)
-    print(f'savgol={savgol}')
-    mk_wav({'num_waveforms': wa, 'num_samples': sa, 'savgol': savgol})
-    mk_wt({'num_waveforms': wa, 'num_samples': sa, 'savgol': savgol})
-    mk_h2p({'savgol': savgol})
+    print('savgol')
+    bdict = {'num_waveforms': wa, 'num_samples': sa, 'savgol': (10, 3)}
+    mk_wav(bdict)
+    mk_wt(bdict)
+    mk_h2p(bdict)
 
 def gen_gauss():
-    ga = 40
-    print(f'gauss={ga}')
-    mk_wav({'num_waveforms': wa, 'num_samples': sa, 'gauss': ga})
-    mk_wt({'num_waveforms': wa, 'num_samples': sa, 'gauss': ga})
-    mk_h2p({'gauss': ga})
+    print('gauss')
+    bdict = {'num_waveforms': wa, 'num_samples': sa, 'gauss': 40}
+    mk_wav(bdict)
+    mk_wt(bdict)
+    mk_h2p(bdict)
 
 def gen_bitcrush():
     print('bitcrush')
     for bc in [4, 5, 6]:
-        mk_wav({'num_waveforms': wa, 'num_samples': sa, 'bitcrush': bc})
-        mk_wav({'num_waveforms': wa, 'num_samples': sa, 'bitcrush': bc,
-                'dline': True})
-        mk_wt({'num_waveforms': wa, 'num_samples': sa, 'bitcrush': bc})
-        mk_wt({'num_waveforms': wa, 'num_samples': sa, 'bitcrush': bc,
-               'dline': True})
-        mk_h2p({'bitcrush': bc})
-        mk_h2p({'bitcrush': bc, 'dline': True})
+        bdict = {'num_waveforms': wa, 'num_samples': sa, 'bitcrush': bc}
+        mk_wav(bdict)
+        mk_wav(bdict|{'dline': True})
+        mk_wt(bdict)
+        mk_wt(bdict|{'dline': True})
+        mk_h2p(bdict)
+        mk_h2p(bdict|{'dline': True})
 
 def gen_bezier():
     print('variable bezier')
     for o in range(-25,26,25):
         for bz in range(1, -6, -1):
-            #mk_png({'num_waveforms': 64, 'num_samples': 256, 'bezier': bz,
-            #        'mid_yoffset': o})
-            mk_wav({'num_waveforms': wa, 'num_samples': sa, 'bezier': bz,
-                    'mid_yoffset': o})
-            mk_wt({'num_waveforms': wa, 'num_samples': sa, 'bezier': bz,
-                   'mid_yoffset': o})
-            mk_h2p({'bezier': bz, 'mid_yoffset': o})
+            bdict = {'num_waveforms': wa, 'num_samples': sa,
+                     'bezier': bz,'mid_yoffset': o}
+            mk_wav(bdict)
+            mk_wt(bdict)
+            mk_h2p(bdict)
             if o == 0:
                 # all bezier with offset 0 are the same
                 break
@@ -151,44 +131,40 @@ def gen_tanh():
     print('variable tanh')
     for o in [-25, -15, 0, 15, 25]:
         for tanh in range(2, 5, 1):
-            mk_wav({'num_waveforms': wa, 'num_samples': sa,
-                   'tanh': tanh, 'mid_yoffset': o})
-            mk_wt({'num_waveforms': wa, 'num_samples': sa,
-                  'tanh': tanh, 'mid_yoffset': o})
-            mk_h2p({'tanh': tanh, 'mid_yoffset': o})
+            bdict = {'num_waveforms': wa, 'num_samples': sa, 'tanh': tanh,
+                     'mid_yoffset': o}
+            mk_wav(bdict)
+            mk_wt(bdict)
+            mk_h2p(bdict)
 
 def gen_exp():
     print('variable offset/exp')
     for e in range(2, 9, 1):
         for o in [-20, -10, 0, 15, 25]:
-            #mk_png({'num_waveforms': 64, 'num_samples': 256, 'exp': e,
-            #        'mid_yoffset': o})
-            #continue
-            mk_wav({'num_waveforms': wa, 'num_samples': sa, 'exp': e,
-                    'mid_yoffset': o})
-            mk_wt({'num_waveforms': wa, 'num_samples': sa, 'exp': e,
-                   'mid_yoffset': o})
-            mk_h2p({'exp': e, 'mid_yoffset': o})
+            bdict = {'num_waveforms': wa, 'num_samples': sa, 'exp': e,
+                    'mid_yoffset': o}
+            mk_wav(bdict)
+            mk_wt(bdict)
+            mk_h2p(bdict)
 
 def gen_shift():
     print('shift')
-    half = sa // 2
-    mk_wav({'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
-            'shift': half, 'mid_width_pct': 95})
-    mk_wav({'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
-            'shift': half, 'mid_width_pct': 95, 'bezier': 0 })
-    mk_wav({'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
-            'shift': half, 'mid_width_pct': 95, 'dline': True })
-    mk_wt({'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
-            'shift': half, 'mid_width_pct': 95})
-    mk_wt({'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
-            'shift': half, 'mid_width_pct': 95, 'bezier': 0 })
-    mk_wt({'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
-            'shift': half, 'mid_width_pct': 95, 'dline': True })
-    # h2p has fixed 128 samples
-    mk_h2p({'mid_yoffset': 0, 'shift': 64, 'mid_width_pct': 95})
-    mk_h2p({'mid_yoffset': 0, 'shift': 64, 'mid_width_pct': 95, 'bezier': 0})
-    mk_h2p({'mid_yoffset': 0, 'shift': 64, 'mid_width_pct': 95, 'dline': True})
+    bdict = {'num_waveforms': wa, 'num_samples': sa, 'mid_yoffset': 0,
+             'shift': sa // 2, 'mid_width_pct': 99}
+    # bdict['gif'] = True
+    mk_wav(bdict|{'exp': 2})
+    mk_wav(bdict|{'tanh': 1.5})
+    mk_wav(bdict|{'bezier': 0 })
+    mk_wav(bdict|{'dline': True })
+    mk_wt(bdict|{'exp': 2})
+    mk_wt(bdict|{'tanh': 1.5})
+    mk_wt(bdict|{'bezier': 0 })
+    mk_wt(bdict|{'dline': True })
+    bdict['shift'] = 64  # h2p has fixed 128 samples
+    mk_h2p(bdict|{'exp': 2})
+    mk_h2p(bdict|{'tanh': 1.5})
+    mk_h2p(bdict|{'bezier': 0})
+    mk_h2p(bdict|{'dline': True})
 
 gen_shift()
 gen_direct()
